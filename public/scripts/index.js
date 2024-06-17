aac53926c01e5d0217cfbef563957fff4fc914ed
Robin <robin.django@gmail.com>
2024-06-17 11:06:05 +0200

    Color change in cauldron

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,11 +3,21 @@ document
   .querySelectorAll(".ingredients-list input[type=checkbox]")
   .forEach((el) => {
     el.addEventListener("change", function () {
+      if (
+        el.closest(".ingredients-list").querySelectorAll("input:checked")
+          .length == 0
+      ) {
+        document.querySelector(".cauldron svg").style.color = "#545353";
+      } else if (this.checked) {
+        document.querySelector(".cauldron svg").style.color = this.dataset.color;
+      };
+
       if (this.checked) {
         document.querySelector(`.active-grid label[for="${this.id}"]`).classList.remove("display-none")
       } else {
         document.querySelector(`.active-grid label[for="${this.id}"]`).classList.add("display-none")
-      }
+      };
+      
       
 
       if (
